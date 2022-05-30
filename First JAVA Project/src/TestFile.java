6169529ad1ce9239bf47b620c4e977c5cf15d9f9
rvvashistha <rohitvashistha171@gmail.com>
2022-05-30 16:50:14 +0530

    commenting out the string parts

--- First JAVA Project/src/TestFile.java
+++ First JAVA Project/src/TestFile.java
@@ -3,14 +3,15 @@ public class TestFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi rohit is my name !!! hello");
+/*System.out.println("hi rohit is my name !!! hello");
 
 System.out.println("i live in delhi");
 System.out.println("i like to play tennis ");
-
+ 
 System.out.println("i like to read self help books ");
-int a=0; 
-int b=0;
+*/
+long a=0; 
+long b=0;
 System.out.println("please provide integer you want to add :-  ");
 Scanner sc=new Scanner(System.in);
 a=sc.nextInt();
