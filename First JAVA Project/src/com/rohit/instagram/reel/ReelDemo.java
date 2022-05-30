0a2df580dcba9777e415b9384b2d2f5be515c7bf
rvvashistha <rohitvashistha171@gmail.com>
2022-05-30 23:58:42 +0530

    package added

---
+++ First JAVA Project/src/com/rohit/instagram/reel/ReelDemo.java
@@ -0,0 +1,10 @@
+package com.rohit.instagram.reel;
+
+public class ReelDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ First JAVA Project/src/com/rohit/instagram/story/StoryDemo.java
@@ -0,0 +1,10 @@
+package com.rohit.instagram.story;
+
+public class StoryDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+	}
+
+}
