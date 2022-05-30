e4eb33b4c212c4fe92aacb06f99c22d1095eea34
rvvashistha <rohitvashistha171@gmail.com>
2022-05-30 16:34:38 +0530

    sum application added!!

--- First JAVA Project/src/TestFile.java
+++ First JAVA Project/src/TestFile.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class TestFile {
 
 	public static void main(String[] args) {
@@ -8,7 +8,21 @@ System.out.println("hi rohit is my name !!! hello");
 System.out.println("i live in delhi");
 System.out.println("i like to play tennis ");
 
-System.out.println("i like to read self help books ");}
+System.out.println("i like to read self help books ");
+int a=0; 
+int b=0;
+System.out.println("please provide integer you want to add :-  ");
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+System.out.println("your sum of two integer are :" +(a+b));
+
+
+	
+	
+	
+	
+	}
 	
 	
 
