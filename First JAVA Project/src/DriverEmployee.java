2d5fb17c3d96162811938e8d03685125de295f16
rvvashistha <rohitvashistha171@gmail.com>
2022-05-31 13:40:32 +0530

    new commit

---
+++ First JAVA Project/src/DriverEmployee.java
@@ -0,0 +1,27 @@
+
+public class DriverEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		Employee emp1= new Employee();
+		emp1.empId=123;
+		emp1.empName="rohit";
+		emp1.empAddress="delhi";
+		emp1.empFirstName="Rohit";
+		emp1.empLastName="Vashistha";
+		
+		
+		emp1.displayEmpData();
+        
+		Employee emp2= new Employee();
+		emp1.empId=124;
+		emp1.empName="rahul";
+		emp1.empAddress="delhi";
+		emp1.empFirstName="rahul";
+		emp1.empLastName="kumar";
+		
+ 
+	}
+
+}
