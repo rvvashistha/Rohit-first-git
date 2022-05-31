69f5f26550cd83031bba8ef29ba59fe440da274f
rvvashistha <rohitvashistha171@gmail.com>
2022-05-31 15:33:58 +0530

    new commit

---
+++ First JAVA Project/src/com/rohit/instagram/post/PostDemo.java
@@ -0,0 +1,22 @@
+package com.rohit.instagram.post;
+
+import java.util.Scanner;
+
+import com.rohit.instagram.story.StoryDemo;
+
+public class PostDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    StoryDemo storyobj=new StoryDemo();
+	storyobj.stroyMethod();
+	
+	
+	
+
+	
+	
+	
+	}
+
+}
--- First JAVA Project/src/com/rohit/instagram/story/StoryDemo.java
+++ First JAVA Project/src/com/rohit/instagram/story/StoryDemo.java
@@ -2,9 +2,10 @@ package com.rohit.instagram.story;
 
 public class StoryDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void stroyMethod() {
+		
+		System.out.println("creating instagram story");
 	}
-
+	
+ 
 }
