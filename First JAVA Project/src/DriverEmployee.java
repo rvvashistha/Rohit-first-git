33f2087c2b42711bf18ac3dc5c11a879a40034bb
rvvashistha <106310894+rvvashistha@users.noreply.github.com>
2022-05-31 13:38:18 +0530

    new class employee for object demo

---
+++ First JAVA Project/src/DriverEmployee.java
@@ -0,0 +1,20 @@
+
+public class DriverEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		Employee emp1= new Employee();
+		emp1.empId=123;
+		emp1.empName="rohit";
+		emp1.empAddress="delhi";
+		emp1.empFirstName="Rohit";
+		emp1.empLastName="Vashistha";
+		
+		
+		emp1.displayEmpData();
+        
+ 
+	}
+
+}
