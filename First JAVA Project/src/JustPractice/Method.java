c9112d1b37989c3286e3c7bd8786c1ccbd46e494
rvvashistha <rohitvashistha171@gmail.com>
2022-06-12 12:47:16 +0530

    new

---
+++ First JAVA Project/src/JustPractice/Method.java
@@ -0,0 +1,15 @@
+package JustPractice;
+
+public class Method {
+	int num= 10;
+	int output;
+	public int performleftShift(){
+		output=num <<10;
+		return output;
+	
+	} 
+	
+	void performRightShift(){
+		
+	}
+}
