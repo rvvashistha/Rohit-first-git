6b3e500bfbabc547b1050b88a5b535ea42ea6288
rvvashistha <rohitvashistha171@gmail.com>
2022-06-14 22:52:01 +0530

    hash set commit

---
+++ First JAVA Project/src/hashsetdemo/HashSetDemo.java
@@ -0,0 +1,35 @@
+package hashsetdemo;
+import java.util.*;
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<String> hashset=new HashSet<>();
+		
+		
+		hashset.add("rohit");
+		hashset.add("rahul");
+		hashset.add("karan");
+
+		
+		
+		hashset.add("rohit");
+		hashset.add("rima");
+		hashset.add("kush");
+		
+		
+	//	System.out.println(hashset);
+		
+		Iterator<String> itr= hashset.iterator();
+		
+		while(itr.hasNext()) {
+			
+			System.out.println(itr.next());
+		
+		}
+		System.out.println();
+
+	}
+	
+
+}
