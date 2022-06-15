6f074c2198604cc21f3006f2cc8d71b0bff87583
rvvashistha <rohitvashistha171@gmail.com>
2022-06-15 21:51:51 +0530

    new commit

---
+++ Data Structure And Algorithms/src/binarysearchalgo/BinarySearch.java
@@ -0,0 +1,52 @@
+package binarysearchalgo;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+	public static void binarysearch(int[]arr,int first,int last, int key){
+		int mid=(first+last)/2;
+		
+		while(first<=last) {
+			
+			if(arr[mid]<key) {
+				first=mid+1;
+				
+			}
+			else if (arr[mid]==key){
+				
+				System.out.println("element found at index: "+mid);
+	            	break;
+				
+			}
+			else {
+				last=mid-1;
+				}
+			mid=(first+last)/2;
+			}
+		if(first>last) {
+			System.out.println("element not found!!!!");
+		}
+	}     
+	   
+	public static void main (String[] args) {
+		
+		int arr[]= {10,20,30,40,50,60,70,80,90,100};
+		
+	   System.out.println("please enter the integer whose index value you want to check  : ");
+	     Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+        int key=a;
+        int last=arr.length-1;
+        binarysearch(arr, 0, last, key);
+		
+	  
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
